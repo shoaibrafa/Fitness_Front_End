c6c5f7831608851921a5d7a78037948500cc39b3
ahmadrafa <shoaibrafa@gmail.com>
2022-02-15 19:24:37 +0530

    Added the search component and now it shows the search results.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,8 @@
+import { ClientModel } from './../categories/client/client.model';
+import { ClientService } from './../categories/client/client-service/client.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-search',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  /**
+  * Create a field to hold the clients information.
+  */
+  clients: ClientModel[];
+
+  constructor(private route: ActivatedRoute,
+    private service: ClientService) { }
+
 
   ngOnInit(): void {
+
+    this.route.params.subscribe(params => {
+      this.search(params['searchKeyword']);
+    });
+
   }
+
+  search(searchKeyword: string): void {
+    this.service.fetchSearch(searchKeyword).subscribe(data => {
+      this.clients = data;
+      console.log(data)
+    });
+  }
+
+
+  statusStyle(status: string): string {
+    if (status === 'Active') {
+      return 'text-success';
+    } else {
+      return 'text-danger';
+    }
+  }
+
+
+
 
 }
